6441f52dfb9cdc02571b567a4ba0310f7d89d091
roluochKe <rasholuoch@gmail.com>
2021-11-22 14:53:33 +0300

    2. Subjects, Shared Examples, and Shared Context

---
+++ rspec-tutorial/spec/shared_examples_spec.rb
@@ -0,0 +1,31 @@
+RSpec.shared_examples 'a Ruby object with 3 elements' do
+  it 'returns the number of items' do
+    expect(subject.length).to eq(3)
+  end
+end
+
+RSpec.describe Array do
+  subject { [1, 2, 3] }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+RSpec.describe String do
+  subject { 'abc' }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+RSpec.describe Hash do
+  subject { {a: 1, b:2, c:3 } }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+class SuasageLink
+  def length
+    3
+  end
+end
+
+RSpec.describe SuasageLink do
+  subject { described_class.new }
+  include_examples 'a Ruby object with 3 elements'
+end
